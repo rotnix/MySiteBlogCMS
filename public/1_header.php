e6e3e48134bef569fb8c71a191fd94365e26145e
rotnix <rotnix@gmail.com>
2014-10-08 19:26:25 +0200

    Reworked Layout and styles

---
+++ public/1_header.php
@@ -0,0 +1,50 @@
+<?php require_once("../includes/initialize.php"); ?>
+
+<!-- Gets Pagination 2 Post -->
+<?php include('interface_article_pagination_2_catchpost.php') ?>	
+
+<!DOCTYPE HTML>
+<html>
+
+    <head>
+    	
+    	<!--<link rel="icon" type="image/png" href="http://example.com/myicon.png">-->
+    	
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="stylesheets/style_main.css" />
+         
+             		
+		<!--Interface Includes -->
+		<link rel="stylesheet" type="text/css" href="stylesheets/style_form_button_delete.css" />
+       	<link rel="stylesheet" type="text/css" href="stylesheets/style_form_input.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheets/style_form_input.css" />
+		
+		
+		<!--Other Includes -->
+        <link rel="stylesheet" type="text/css" href="stylesheets/style_picture_round.css" />
+        
+		<link rel="stylesheet" type="text/css" href="stylesheets/style_category_link.css" />
+
+   </head>
+<body>
+		
+	<div id="blurbackground-menu">
+	</div>
+	<div id="safenodeOverlay-menu">
+	</div>
+
+<div id="main-container">
+	
+	<!-- Menu Include -->
+	<?php include('interface_menu_left_1.php') ?>			
+	
+	<!-- Feauters button right include -->
+	<?php include('interface_features.php') ?>		
+
+<!-- Warps Site conntend &resets position  -->
+<div id="content-wrapper"> 
+		
+	<!--ContentColum  &offsets layout by width off nav-->
+	<div id="content-container">
+		<!----------------Main-Article-Container------------------>
+		<article>
